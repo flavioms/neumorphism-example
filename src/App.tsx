37ecba93ba07c556d3de583166bbdeaef42a5edd
unknown <flaviomartins_silva@hotmail.com>
2020-03-30 18:23:40 -0300

    Add persisted state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
-import { ThemeProvider } from "styled-components";
+import React from "react";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import usePersistedState from "./utils/usePersistededState";
 import light from "./styles/themes/light";
 import dark from "./styles/themes/dark";
 import GlobalStyle from "./styles/global";
 import Header from "./components/Header";
 
 function App() {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistedState<DefaultTheme>("theme", light);
   const toggleTheme = () => {
     setTheme(theme.title === "light" ? dark : light);
   };
